e99aae22cead1f99d07be417cd3baa7eb1987b85
bagusstriutomo <anandabagus121@gmail.com>
2025-10-19 06:02:03 +0700

    feat: add MyOrderController to develop

---
+++ app/Http/Controllers/MyOrderController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MyOrderController extends Controller
+{
+    public function index(){
+        $orders = Order::where('user_id', auth()->id())->latest()->paginate(10);
+        return view('shop.my-orders', compact('orders'));
+    }
+    public function show(Order $order){
+        abort_unless($order->user_id===auth()->id(), 403);
+        $order->load('items');
+        return view('shop.my-order-show', compact('order'));
+    }
+
+}
